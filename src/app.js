53c30fd2d79677478fb238eb9ceae1963edcd7bf
Arkajyoti Kundu <arkajyotikundu415@gmail.com>
2024-05-02 09:58:30 +0530

    create an error and configuration  env

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-import dotenv from "dotenv";
 import { Envconfig } from './config/config.js';
+import createHttpError from 'http-errors';
+import globalErrorHandler from './middlewers/globalErrorHandler.js'
+import userRouter from './routes/user.routes.js';
+
+import dotenv from "dotenv";
 dotenv.config({ path: './.env' });
 
 // console.log(process.env);
@@ -13,25 +17,35 @@ dotenv.config({ path: './.env' });
 const app=express();
 
 
-app.use(express.json());
+app.use(express.json({limit:"21kb"}));
 
-//  console.log(`${process.env.CORS_ORIGIN}`);
 app.use(cors({
     origin:Envconfig.corsOrigin,
     credentials: true
 }));
-app.use(express.json({limit:"21kb"}));
+
 app.use(express.urlencoded({
     extended:true,
     limit:"21kb",
 }))
 app.use(cookieParser());
 app.use(express.static("public"));
+
 app.get('/',(req,res)=>{
-   res.json({message:"Response sent successfully from app.js"})
+
+ const error= createHttpError(400,"Something went to wrong")
+//  throw error;
+
+   res.json({message:"  Wellcome to Home page !!!"})
+
 })
 
+app.use('/api/user',userRouter)
 
 
+
+// Global error handeler
+
+app.use(globalErrorHandler);
 
 export default app;
